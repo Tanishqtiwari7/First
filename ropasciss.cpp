3bbe0ca44a9b53e365ee8b63aefe22d3604e7400
SanaTanishq <tanishqtiwari2020@gmail.com>
2023-12-13 20:05:51 +0530

    function

--- ropasciss.cpp
+++ ropasciss.cpp
@@ -1,28 +1,68 @@
 #include <iostream>
 #include <ctime>
 
-char userin ();
-
-
-int main()
-{
-    // =
-    userin();
-}
-
-
 char userin()
 {
     char input;
     std::cout<<"\t\tROCK PAPER SCISSORS\n";
     std::cout<<"\t\t*******GAME********\n\n";
-
+// do{
     std::cout<<"choose any of the actions:\n";
     std::cout<<"1. 'r' for rock\n";
-    std::cout<<"2. 'r' for paper\n";
-    std::cout<<"3. 'r' for scissors\n";
+    std::cout<<"2. 'p' for paper\n";
+    std::cout<<"3. 's' for scissors\n";
     std::cin>> input;
-    std::cout<<input;
+     return input;
+// } while (input == r) &&);
     
 }
+
+char comp()
+{
+
+    char com;
+    int compu;
+    compu = (rand()%3)+1;
+
+    switch (com)
+    {
+    case 1: com == 'r';
+        
+        break;
+
+        case 2 : com == 'p';
+        break;
+        case 3: com =='s';
+        break;
+    
+   
+    }
+    return com;
+
+}
+
+
+int main()
+{
+    // =
+
+    char x,y;
+    x=userin();
+    y=comp();
+
+    if (x==y)
+    {
+       std::cout<<"you win!\n";
+
+    }
+    else 
+    {
+        std::cout<<"you loose!\n";
+    }
+    
+
+
+}
+
+
 
