046caea54368deafe20db48549ae8ada78723c70
SanaTanishq <tanishqtiwari2020@gmail.com>
2023-12-14 12:26:27 +0530

    jnj

--- testing.cpp
+++ testing.cpp
@@ -33,7 +33,7 @@
 // // // // int i=0;
 // // // // while (i==y)
 // // // //     {
-       
+
 // // // //     std::cout<<i;
 // // // //        i++;
 // // // //     }
@@ -76,12 +76,10 @@
 // //     std::cout<<"*****************To the game*****************\n";
 // //     std::cout<<"***********************************************\n\n\n";
 
-        
-    
 // //     std::cout<<"For adding your crazy dares enter : 1\n";
 // //     std::cout<<"For having default numbers enter : 2\n";
 // //     std::cin>>z;
-    
+
 // //     if (z==1)
 // //     {
 // //     std::cout<<"How many sections you want on the the wheel of the hell:-";
@@ -108,21 +106,18 @@
 // //     std::cout<<"How many sections you want on the the wheel of the hell:-";
 // //     std::cin>>y;
 // //     int x = (rand() % y)+1;
-// //     std::cout<<"Your no. is :-"<<x;    
+// //     std::cout<<"Your no. is :-"<<x;
 // //    }
 // //    else
 // //    {
-   
+
 // //    do{
-    
-            
-    
+
 // //         std::cout<<"Enter the correct no.:-";
 // //         std::cin>>z;
-    
-   
+
 // //    }while(z!=1||z!=2);
-// //    }    
+// //    }
 // //     std::cout<<"Do you want to play this game again(y/n)";
 // //     std::cin>>a;
 // //     // while(a!=1||a!=y||a!=2||a!=N)
@@ -130,10 +125,10 @@
 // //     if (a!=y||a!=n||a!=Y||a!=N)
 // //     {
 // //         do{
-// //        std::cout<<"Enter the correct alphabet"; 
+// //        std::cout<<"Enter the correct alphabet";
 // //         }while(a!=y||a!=n||a!=Y||a!=N);
 // //     }
-    
+
 // // }while(a==y||a==Y);
 // // }
 // // for (size_t i = 0; i < count; i++)
@@ -141,7 +136,6 @@
 // //     /* code */
 // // }
 
-    
 // // switch (expression)
 // // {
 // // case /* constant-expression */:
@@ -157,4 +151,29 @@
 // int main()
 // {
 //     cout<<void tata();
+// // }
+// #include <iostream>
+
+// void comp()
+// {
+
+//     char com;
+//     int compu = 2;
+//     // compu = (rand()%3)+1;
+
+
+        // std::cout << "the compuert choose ' r '\n";
+
+        // break;
+
+//  if(compu ==2)
+//         {std::cout << "the compuert choose ' p '\n";
+//         com == 'p';}
+//     //     break;}
+//     // case 3:
+//     //     com == 's';
+    //     std::cout << "the compuert choose ' s '\n";
+    //     break;
+    // }
+   
 // }
